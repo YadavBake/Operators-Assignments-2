3a94763952c45d20ac3371dc6982d916a47cb9d7
yadav Bakelal <bake2101992@gmail.com>
2023-11-16 18:37:44 +0530

    Update Operators & Assignments Part-6  operator & operand precedence.java

--- Operators & Assignments Part-6  operator & operand precedence.java
+++ Operators & Assignments Part-6  operator & operand precedence.java
@@ -1,6 +1,6 @@
 
-															Operators & Assignments Part-6 || operator & operand precedence
-														=====================================================================
+											Operators & Assignments Part-6 || operator & operand precedence
+									 	     =====================================================================
 
 ----------------------------
   14. operator precendence. 
@@ -10,14 +10,14 @@
  
  1. Unary operators:		2. Arithmentic operators:		3. shift operators:  	4. Comparison operators:			5. equality operators:		6. Bitwise Operators:
                                                                                                                                                     
-	[], x++,x--             	*,/,%                       	>> , >>>, <<        	<, <=, >, >=, instanceof        	==, !=                  	&
-	++x, --X,~,!            	+,-                         	                    	                                                            	^
-	new, <type>                                                                                                                                     	|
+	[], x++,x--             	*,/,%                       	>> , >>>, <<        	<, <=, >, >=, instanceof        			==, !=                  	&
+	++x, --X,~,!            	+,-                         	                    	                                                            				^
+	new, <type>                                                                                                                                     				|
 	
   
 7. Short circuti operators:		8. Conditional operators:		9. Assignment operators:
                                                                 
-	&&                          	?,:                         	=, +=, -=, *=.....
+	&&                          	?,:                         		=, +=, -=, *=.....
 	||
 	
 
@@ -38,9 +38,9 @@
 				}
 			public static int m1(int i) {							output
 				                                                    
-					System.out.println(i);                          1+2*3/4+5*6		1
-					return i;                                       1+6/4+5*6		2
-			}                                                       1+1+5*6			3
+					System.out.println(i);              1+2*3/4+5*6			1
+					return i;                           1+6/4+5*6			2
+			}                                                   1+1+5*6			3
 		}                                                           1+1+30			4
 		                                                            2+30			5	
 		                                                            32				6
